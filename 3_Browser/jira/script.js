355436cf6439aa88fbffb4e9c49291064aaa5b2a
mohd-aman <saifiamaan445@gmail.com>
2022-04-11 23:25:09 +0530

    addBtn functionality added

--- 3_Browser/jira/script.js
+++ 3_Browser/jira/script.js
@@ -1,6 +1,13 @@
 let addBtn = document.querySelector(".add-btn");
+let addModal = true;
+let modal = document.querySelector(".modal-cont");
 
-addBtn.addEventListener("click",function(e){
+addBtn.addEventListener("click",function(){
     //Display a Modal
-    console.log("Button was clicked")
+    if(addModal){
+        modal.style.display = "flex";
+    }else{
+        modal.style.display = "none";
+    }
+    addModal = !addModal
 })
