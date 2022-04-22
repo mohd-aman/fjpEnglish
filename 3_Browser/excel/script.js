ae1882104561077b6f8c1e4a8971960e3cc13bd1
mohd-aman <saifiamaan445@gmail.com>
2022-04-22 22:26:40 +0530

    cells positioning done

--- 3_Browser/excel/script.js
+++ 3_Browser/excel/script.js
@@ -1,15 +1,26 @@
-let cellsContent = document.querySelector(".cells-content");
+let cellsContentDiv = document.querySelector(".cells-content");
 
 function initCells(){
-    let cells = "";
-    for(let i=0;i<100;i++){
-        cells+="<div class='row'"
-        for(let j=0;j<26;j++){
-            cells+="<div class = 'cell'>Cell</div>"
-        }
-        cells+="</div>"
+    let cellsContent = "<div class = 'top-left-cell'></div>";
+    cellsContent+="<div class='top-row'>"
+    for(let i=0;i<26;i++){
+        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`
     }
-    cellsContent.innerHTML = cells;
+    cellsContent+="</div>"
+    cellsContent+="<div class = 'left-col'>"
+    for(let i=0;i<100;i++){
+        cellsContent+=`<div class= 'left-col-cell'>${i+1}</div>`
+    }
+    cellsContent+="</div>"
+    cellsContent+="<div class = 'cells'>"
+    for(let i=0;i<100;i++){
+        cellsContent+="<div class='row'>"
+        for(let j=0;j<26;j++){
+            cellsContent+="<div class = 'cell' contentEditable='true'>Cell</div>"
+        }
+        cellsContent+="</div>"
+    }
+    cellsContentDiv.innerHTML = cellsContent;
 }
 
 initCells();
