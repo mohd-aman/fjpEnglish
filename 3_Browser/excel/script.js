f8d295cbe4bf419ebe9e6126f80204aaa07b9cde
mohd-aman <saifiamaan445@gmail.com>
2022-04-22 22:55:15 +0530

    cells ui done top left fixed

--- 3_Browser/excel/script.js
+++ 3_Browser/excel/script.js
@@ -16,7 +16,7 @@ function initCells(){
     for(let i=0;i<100;i++){
         cellsContent+="<div class='row'>"
         for(let j=0;j<26;j++){
-            cellsContent+="<div class = 'cell' contentEditable='true'>Cell</div>"
+            cellsContent+="<div class = 'cell' contentEditable='true'></div>"
         }
         cellsContent+="</div>"
     }
@@ -24,3 +24,16 @@ function initCells(){
 }
 
 initCells();
+
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+
+cellsContentDiv.addEventListener("scroll",function(e){
+    let top = e.target.scrollTop;
+    let left = e.target.scrollLeft;
+    topRow.style.top = top+"px";
+    topLeftCell.style.top = top+"px";
+    topLeftCell.style.left = left+"px";
+    leftCol.style.left = left+"px";
+})
